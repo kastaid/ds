50281cbec88b1e3653dbb0bcfe99f7ceaa717f27
illvart <illvart@protonmail.com>
2026-03-19 04:00:35 +0700

    update

--- ds/config.py
+++ ds/config.py
@@ -1,39 +1,39 @@
+# ruff: noqa: RUF012
 # Copyright (C) 2023-present kastaid
 # https://github.com/kastaid/ds
 # MIT License
 
 from os import getenv
-from typing import ClassVar
 
-from dotenv import find_dotenv, load_dotenv
+from dotenv import load_dotenv
 
-from . import WORKERS
+from . import WORKERS, Root
 
-load_dotenv(find_dotenv())
+load_dotenv(Root / ".env", override=True)
 
 
-def tobool(val: str) -> int | None:
-    """
-    Convert a string representation of truth to true (1) or false (0).
-    https://github.com/python/cpython/blob/main/Lib/distutils/util.py
-    """
-    val = val.lower()
-    if val in {"y", "yes", "t", "true", "on", "1"}:
-        return 1
-    if val in {"n", "no", "f", "false", "off", "0"}:
-        return 0
-    raise ValueError(f"invalid truth value {val!r}")
+def env(key: str, default: str = "") -> str:
+    return getenv(key, default).strip()
+
+
+def to_bool(value: str) -> bool:
+    value = value.lower()
+    if value in {"y", "yes", "t", "true", "on", "1", "enable", "enabled"}:
+        return True
+    if value in {"n", "no", "f", "false", "off", "0", "disable", "disabled"}:
+        return False
+    raise ValueError(f"Invalid boolean value: {value!r}")
 
 
 class Var:
-    DEV_MODE: bool = tobool(getenv("DEV_MODE", "false").strip())
-    API_ID: int = int(getenv("API_ID", "0").strip())
-    API_HASH: str = getenv("API_HASH", "").strip()
-    STRING_SESSION: str = getenv("STRING_SESSION", "").strip()
-    WORKERS: int = int(getenv("WORKERS", str(WORKERS)).strip())
-    HANDLER: str = getenv("HANDLER", "").strip()
+    DEV_MODE: bool = to_bool(env("DEV_MODE", "false"))
+    API_ID: int = int(env("API_ID", "0"))
+    API_HASH: str = env("API_HASH", "")
+    STRING_SESSION: str = env("STRING_SESSION", "")
+    HANDLER: str = env("HANDLER", "")
+    WORKERS: int = int(env("WORKERS", str(WORKERS)))
     IS_STARTUP: bool = False
-    ERROR_RETRY: ClassVar[dict[int, int]] = {}
+    ERROR_RETRY: dict[int, int] = {}
 
 
-del load_dotenv, find_dotenv, WORKERS
+del load_dotenv, WORKERS, Root
